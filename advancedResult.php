2e193de3fcb79cff420b5e0a0e8a8a1d734d1318
Albert Cieslak <albert.cieslak2@gmail.com>
2018-01-02 22:19:55 +0100

    :D

--- advancedResult.php
+++ advancedResult.php
@@ -6,52 +6,88 @@
 
 <h3>Składy drużyn</h3>
 <table border="1">
-<?php
-$link = pg_connect("host=localhost dbname=postgres user=postgres password=postgres");
-$match = $_GET["mecz"];
-$players1 = pg_query($link,
-    "SELECT imie,nazwisko FROM zawodnik_sklad
+    <?php
+    $link = pg_connect("host=localhost dbname=postgres user=postgres password=postgres");
+    $match = $_GET["mecz"];
+    $players1 = pg_query($link,
+        "SELECT imie,nazwisko FROM zawodnik_sklad
 JOIN zawodnik ON zawodnik_sklad.id_zawodnik = zawodnik.id
 WHERE id_sklad =  (SELECT sklad_1 FROM mecz WHERE mecz.id = $match)");
-$players2 = pg_query($link,
-    "SELECT imie,nazwisko FROM zawodnik_sklad
+    $players2 = pg_query($link,
+        "SELECT imie,nazwisko FROM zawodnik_sklad
 JOIN zawodnik ON zawodnik_sklad.id_zawodnik = zawodnik.id
 WHERE id_sklad =  (SELECT sklad_2 FROM mecz WHERE mecz.id = $match)");
-$teamName1 = pg_query("SELECT nazwa FROM mecz
+    $teamName1 = pg_query("SELECT nazwa FROM mecz
   JOIN sklad ON mecz.sklad_1 = sklad.id
   JOIN druzyna ON sklad.druzyna = druzyna.id
-WHERE mecz.id = 1");
-$teamName2 = pg_query("SELECT nazwa FROM mecz
+WHERE mecz.id = $match");
+    $teamName2 = pg_query("SELECT nazwa FROM mecz
   JOIN sklad ON mecz.sklad_2 = sklad.id
   JOIN druzyna ON sklad.druzyna = druzyna.id
-WHERE mecz.id = 1");
+WHERE mecz.id = $match");
+    $sets = pg_query("SELECT wynik1,wynik2 FROM set
+WHERE mecz = $match");
+    $winner = pg_query("SELECT nazwa FROM mecz
+  JOIN druzyna ON druzyna.id = zwyciezca
+WHERE mecz.id=$match");
 
-$numrows = 6;
-$teamName1 = pg_fetch_array($teamName1, 0)["nazwa"];
-$teamName2 = pg_fetch_array($teamName2, 0)["nazwa"];
-echo "<tr><th>$teamName1</th></tr>";
-for($ri = 0; $ri < $numrows; $ri++) {
-    echo "<tr>\n";
-    $row = pg_fetch_array($players1, $ri);
-    echo " <td>" . $row["imie"] . " " . $row["nazwisko"] . "</td>
-
-    </tr>
-    ";
-}
-echo "</table>";
-echo "<table border=\"1\">";
-echo "<tr><th>$teamName2</th></tr>";
-for($ri = 0; $ri < $numrows; $ri++) {
-    echo "<tr>\n";
-    $row = pg_fetch_array($players2, $ri);
-    echo " <td>" . $row["imie"] . " " . $row["nazwisko"] . "</td>
+    $numrows = 6;
+    $teamName1 = pg_fetch_array($teamName1, 0)["nazwa"];
+    $teamName2 = pg_fetch_array($teamName2, 0)["nazwa"];
+    $winner = pg_fetch_array($winner,0)["nazwa"];
+    echo "<tr><th>$teamName1</th></tr>";
+    for ($ri = 0; $ri < $numrows; $ri++) {
+        echo "<tr>\n";
+        $row = pg_fetch_array($players1, $ri);
+        echo " <td>" . $row["imie"] . " " . $row["nazwisko"] . "</td>
 
     </tr>
     ";
-}
-echo "</table>\n";
-pg_close($link);
-?>
+    }
+    ?>
+</table>
+<br>
+<table border="1">
+    <?php
+    echo "<tr><th>$teamName2</th></tr>";
+    for ($ri = 0; $ri < $numrows; $ri++) {
+        echo "<tr>\n";
+        $row = pg_fetch_array($players2, $ri);
+        echo " <td>" . $row["imie"] . " " . $row["nazwisko"] . "</td>
+
+    </tr>
+    ";
+    }
+    ?>
+
+</table>
+<br>
+<?php
+    if ($winner == "Mecz nierozegrany") {
+        echo "<h3>Mecz nierozegrany</h3>";
+    } else {
+        echo "
+            <h4>Zwyciezyli: $winner</h4>
+            <h3>Wyniki odbytych setow</h3>
+            <table border=\"1\">
+            <tr>
+                <th>$teamName1</th>
+                <th>$teamName2</th>
+            </tr>
+        ";
+        for ($ri = 0; $ri < pg_numrows($sets); $ri++) {
+            echo "<tr>\n";
+            $row = pg_fetch_array($sets, $ri);
+            echo " <td>" . $row["wynik1"] . "</td>
+            <td>" . $row["wynik2"] . "</td>
+
+            </tr>
+            ";
+        }
+        echo "</table>";
+    }
+    pg_close($link);
+    ?>
 
 </body>
 </html>
--- result.php
+++ result.php
@@ -1,9 +1,13 @@
-<h2 align=center>Rozegrane mecze:</h2>
+<?php
+
+?>
+<h2 align=center>Mecze:</h2>
 
 <table border="1" align=center>
     <tr>
         <th>Pierwsza drużyna</th>
         <th>Druga drużyna</th>
+        <th>Wynik</th>
         <th>Zwycięzca</th>
     </tr>
     <?php
@@ -11,11 +15,21 @@
     for($ri = 0; $ri < $numrows; $ri++) {
         echo "<tr>\n";
         $row = pg_fetch_array($result, $ri);
-        $id = $row["id"];
+        $match = $row["id"];
+        $points1 = pg_fetch_array(pg_query($link,
+            "SELECT COUNT(*) wynik FROM set 
+            WHERE mecz = $match AND set.wynik1 > set.wynik2"),0)["wynik"];
+        $points2 = pg_fetch_array(pg_query($link,
+            "SELECT COUNT(*) wynik FROM set 
+            WHERE mecz = $match AND set.wynik1 < set.wynik2"),0)["wynik"];
+        $points = $points1 . " : " . $points2;
+        if ($row["zwyciezca"] == "Mecz nierozegrany")
+            $points = "";
         echo " <td>" . $row["nazwa1"] . "</td>
      <td>" . $row["nazwa2"] . "</td>
+     <td>" . $points . "</td>
      <td>" . $row["zwyciezca"] . "</td>
-     <td> <a href=\"advancedResult.php?mecz=$id\">Szczegoly meczu</a> </td>
+     <td> <a href=\"advancedResult.php?mecz=$match\">Szczegoly meczu</a> </td>
 
     </tr>
     ";
